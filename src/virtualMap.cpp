7723cbde3b725f777062c3a6ad88b94e48fc93da
RoelYoon <roel.yoon@ggq.gg>
2023-11-13 20:33:24 +0900

    Updated comment -> explains initializeMap()

--- src/virtualMap.cpp
+++ src/virtualMap.cpp
@@ -1,9 +1,10 @@
 #include "deliveryRobot.hpp"
+// Map and Position definitions
 /*
- * virtual mapping:
- * 0 — unmarked/open spot
- * 1 — obstructed
- * 2+ — destination #(n-2)
+   virtual mapping:
+   0 — unmarked/open spot
+   1 - obstacle
+   2+ — destination #(n-2)
 */
 void Map::mark(int row, int col, bool isObstacle){
     int val = isObstacle?1:destinationCount+2;
@@ -20,7 +21,7 @@ Map::Proxy Map::operator[](int r){
     return Map::Proxy(container[r]);
 }
 Position::Position(int r, int c):row(r),col(c){}
-//selects destination based on controller input
+//  selects destination based on controller input
 void setDestination(){
 	if(master.get_digital_new_press(DIGITAL_UP)){destination=2;}
 	else if(master.get_digital_new_press(DIGITAL_LEFT)){destination=3;}
@@ -31,6 +32,11 @@ void setDestination(){
 	else if(master.get_digital_new_press(DIGITAL_A)){destination=8;}
 	else if(master.get_digital_new_press(DIGITAL_B)){destination=9;}
 }
+/* 
+   Hard-code destination positions here. 
+   Marking any obstacles here is not necessary, as that will 
+   be done dynamically during run-time. 
+*/
 void initializeMap(Map &map){
     map.mark(0,0,0); //destination #1
     map.mark(10,3,0); //destination #2
